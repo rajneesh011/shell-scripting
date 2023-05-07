aa283c591f9bfd9c231015aad090c81e2fef13fa
rajneesh011 <rajneeshpratapsingh54@gmail.com>
2023-05-07 21:02:55 +0530

    operators

---
+++ operators.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Arithmetic operators example
+
+num1=10
+num2=5
+
+echo "num1 = $num1"
+echo "num2 = $num2"
+
+# Addition
+result=$((num1 + num2))
+echo "num1 + num2 = $result"
+
+# Subtraction
+result=$((num1 - num2))
+echo "num1 - num2 = $result"
+
+# Multiplication
+result=$((num1 * num2))
+echo "num1 * num2 = $result"
+
+# Division
+result=$((num1 / num2))
+echo "num1 / num2 = $result"
+
+# Modulo
+result=$((num1 % num2))
+echo "num1 % num2 = $result"
+
